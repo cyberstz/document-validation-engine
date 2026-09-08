5fff1a1a3080875207a6107a681583afcc629128
Stanislav Kovalenko <skizzord@gmail.com>
2026-09-08 05:36:10 +0200

    Add validator class

---
+++ src/Validator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DocumentValidation;
+
+use Throwable;
+
+final readonly class Validator
+{
+    public function __construct(private RuleProvider $ruleProvider) {}
+
+    public function validate(Document $document): ValidationResult
+    {
+        $ruleResults = [];
+
+        foreach ($this->ruleProvider->rulesFor($document->tenantId) as $rule) {
+            $ruleResults[] = $this->evaluate($rule, $document);
+        }
+
+        return ValidationResult::from($ruleResults);
+    }
+
+    private function evaluate(ValidationRule $rule, Document $document): RuleResult
+    {
+        try {
+            return $rule->validate($document);
+        } catch (Throwable $exception) {
+            return RuleResult::fail(
+                $rule->type(),
+                sprintf(
+                    'Rule "%s" could not be evaluated (%s): %s',
+                    $rule->type(),
+                    $exception::class,
+                    $exception->getMessage(),
+                ),
+            );
+        }
+    }
+}
